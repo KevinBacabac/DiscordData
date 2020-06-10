229958a53ccaab2285022eaf193454af71144782
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2020-06-10 13:31:40 -0400

    Experiment with multiple datetime formats within column

--- resources/df_tools.py
+++ resources/df_tools.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
 import resources.tz
 
 
@@ -11,18 +12,63 @@ def count_df(df, interval='D', col='timestamp'):
     return pd.DataFrame({col: series.index, 'Count': count})
 
 
-def count_timestamp(df, localized=False, unix=True, interval='D', col='timestamp'):
+def count_timestamp(df, localized=False, unix=False, interval='D', col='timestamp'):
     """Count number of events per day within dataframe. Mutates argument."""
 
     PRUNE = set(df.columns) - {col, }
     df.drop(PRUNE, axis=1, inplace=True)
 
-    if unix:
-        # Keep unix timestamps
-        df[col] = df[col][df[col].apply(isinstance, args=(int,))]
-        df[col] = pd.to_datetime(df[col], unit='ms')
-    else:
-        df[col] = pd.to_datetime(df[col])
+    print(f"count_timestamp. col = {col}")
+    print(df[col])
+
+    if not is_datetime(df[col].dtypes):
+        ifilt = df[col].apply(isinstance, args=(int,))
+        #print(df.loc[~ifilt])
+
+        sfilt = df[col].apply(isinstance, args=(str,))
+        dfilt = ~(ifilt | sfilt)
+        #print("dfilt\n", df.loc[dfilt], "\ndone dfilt")
+
+        I = 0
+
+        df['temp_timestamp'] = pd.to_datetime(0, unit='ms')
+        df['temp_timestamp'] = df['temp_timestamp'].astype('datetime64[ns]')
+
+        #print("test\n", df.iloc[I])
+        df.loc[ifilt, ['temp_timestamp']] = pd.to_datetime(df.loc[ifilt, col], unit='ms')
+        print("ifilt\n", df.loc[ifilt, ['temp_timestamp']])
+        print(df.iloc[0].dtypes)
+        #print("test2\n", df.iloc[I])
+        df.loc[sfilt, ['temp_timestamp']] = pd.to_datetime(df.loc[sfilt, col].str.replace('"', ''))
+        print("sfilt\n", df.loc[sfilt, ['temp_timestamp']])
+        print(df.iloc[0].dtypes)
+        #print("test3\n", df.iloc[I])
+        df.loc[dfilt, ['temp_timestamp']] = pd.to_datetime(df.loc[dfilt, col])
+        print("dfilt\n", df.loc[dfilt, ['temp_timestamp']])
+        print(df.iloc[0].dtypes)
+
+        #print(df['temp_timestamp'])
+        print("test3")
+        df[col] = pd.to_datetime(df['temp_timestamp'], utc=True)
+        print("test4")
+        del df['temp_timestamp']
+
+    print(df[col].dtypes)
+
+    """
+    filt = df[col].apply(isinstance, args=(str,))
+    filt2 = df[col].str.startswith('"', na=False)
+    print(filt)
+    print(filt2)
+    temp_df = pd.to_datetime(df[filt & filt2][col].str.replace('"', ''))
+    print(temp_df)
+    print(type(temp_df))
+
+    df.loc[filt & filt2, col] = temp_df"""
+
+    filt = df[col].apply(isinstance, args=(pd.Timestamp,))
+    print(filt)
+    df.loc[filt, col] = pd.to_datetime(df[filt][col])
 
     if interval == 'D':  # Remove time but keep date
         df[col] = df[col].dt.normalize()
